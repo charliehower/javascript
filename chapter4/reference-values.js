23119b5ee1c0bf777437ce13ed54a0bdb77e7abc
Webmaster <webmaster@Webmasters-iMac.local>
2016-02-09 14:12:31 +0100

    completed chapter 4

---
+++ chapter4/reference-values.js
@@ -0,0 +1,4 @@
+var obj1 = new Object();
+var obj2 = obj1; 
+obj1.name = "Nicholas";
+console.log(obj2.name); //" Nicholas"
---
+++ chapter4/types.js
@@ -0,0 +1,12 @@
+var s = "Nicholas";
+var b = true;
+var i = 22;
+var u;
+var n = null;
+var o = new Object();
+console.log(typeof s); // string
+console.log(typeof b); // boolean
+console.log(typeof i); // number 
+console.log(typeof u); // undefined
+console.log(typeof n); // object
+console.log(typeof o); // object
